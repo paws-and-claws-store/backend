c8389d3f9f5cb4dae3a7a9930374e89d621cf3da
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-08-01 11:13:33 +0300

    Add DataBase

--- models/product.js
+++ models/product.js
@@ -1,15 +1,78 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const productSchema = new Schema({
-  country: {
+  productName: {
+    type: String,
+    required: [true, "Product name is required"],
+  },
+  pet: {
+    ua: String,
+    en: String,
+    code: String,
+  },
+  category: {
+    us: String,
+    ua: String,
+    code: String,
+  },
+  brand: {
+    type: String,
+    required: [true, "Brand is required"],
+  },
+  size: {
+    type: Number,
+    required: [true, "Size is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Size is required"],
+  },
+  sale: {
+    type: Number,
+  },
+  count: {
+    type: Number,
+    required: [true, "Count is required"],
+  },
+  productType: {
+    type: String,
+    required: [true, "Product type is required"],
+  },
+  shortDiscription: {
     type: String,
   },
-  capital: {
+  fullDiscription: {
     type: String,
+  },
+  ingredients: {
+    type: String,
+  },
+  mainImage: {
+    type: String,
+  },
+  images: {
+    type: [String],
+  },
+  favorite: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  reviews: {
+    type: [String],
+  },
+  productCode: {
+    type: String,
+    unique: true,
+  },
+  producingCountry: {
+    ua: String,
+    en: String,
+    code: String,
   },
 });
 
-const Product = model('product', productSchema);
+const Product = model("product", productSchema);
 
 module.exports = {
   Product,
