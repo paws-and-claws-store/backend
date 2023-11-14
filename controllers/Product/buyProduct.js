ff25fdec57dd2cc962efd8ec1dd1753613a2f291
Artem Liaschenko <playfootball826@gmail.com>
2023-11-15 00:54:49 +0200

    basket and details of product

--- controllers/Product/buyProduct.js
+++ controllers/Product/buyProduct.js
@@ -1,28 +1,31 @@
 const { Product } = require("../../models/product");
 const { HttpError, isEnough } = require("../../helpers");
-const consts = require("../../config/consts");
-const { array } = require("joi");
 
 const buyProduct = async (req, res) => {
   const { array } = req.body;
 
   const results = await isEnough(req, res);
+  console.log("asda", results);
 
-  if (results.error) {
-    await Promise.all(
-      array.map(async (el) => {
-        const result = await Product.findOne({ "items.productCode": el.productCode });
-
-        let obj = result.items.find((item) => item.productCode === el.productCode);
-
-        obj.count -= el.cardCount;
-
-        result.save();
-      })
-    );
-  } else {
-    return;
+  if (results.errors.length > 0) {
+    return res.status(400).json({
+      code: 400,
+      data: results.data,
+      errors: results.errors,
+    });
   }
+
+  await Promise.all(
+    array.map(async (el) => {
+      const result = await Product.findOne({ "items.productCode": el.productCode });
+
+      let obj = result.items.find((item) => item.productCode === el.productCode);
+
+      obj.count -= el.cardCount;
+
+      result.save();
+    })
+  );
 
   res.json({
     code: 200,
--- helpers/isEnough.js
+++ helpers/isEnough.js
@@ -1,14 +1,18 @@
 const { Product } = require("../models/product");
+const HttpError = require("./HttpError");
 
 const isEnought = async (req, res) => {
   const { array } = req.body;
   const results = [];
-
   const errorCods = [];
 
   await Promise.all(
     array.map(async (el) => {
       const result = await Product.findOne({ "items.productCode": el.productCode });
+
+      if (!result) {
+        throw HttpError(400, "The product not found");
+      }
 
       const obj = result.items.find((item) => item.productCode === el.productCode);
 
@@ -20,15 +24,10 @@ const isEnought = async (req, res) => {
     })
   );
 
-  if (errorCods.length > 0) {
-    return res.status(400).json({
-      code: 400,
-      data: results,
-      errors: errorCods,
-    });
-  }
-
-  return results;
+  return {
+    data: results,
+    errors: errorCods,
+  };
 };
 
 module.exports = isEnought;
