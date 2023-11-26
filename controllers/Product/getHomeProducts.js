7516b07b93be0ac76a092e564db91bee373971ca
Artem Liaschenko <playfootball826@gmail.com>
2023-11-26 19:19:49 +0200

    fix

--- controllers/Product/getHomeProducts.js
+++ controllers/Product/getHomeProducts.js
@@ -5,9 +5,13 @@ const { LIMIT_PAGINATION } = require("../../config/consts");
 const getHomeProducts = async (req, res) => {
   const { sortBy } = req.query;
 
-  const result = await Product.find({}, { min_sale: 0 })
-    .populate("_pet _category _variant _country")
-    .limit(LIMIT_PAGINATION);
+  const result = await Product.find({}, { min_sale: 0 }).populate(
+    "_pet _category _variant _country"
+  );
+
+  const filterResult = result.map((el) => el);
+
+  console.log("filterResult:", filterResult);
 
   res.json(sort(result));
 };
