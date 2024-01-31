c87a9f6b698c291087b84125620f6a6d5a231025
Artem Liaschenko <playfootball826@gmail.com>
2024-01-31 20:36:09 +0200

    add auth google

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,6 @@
 const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
+const passport = require("./google-authenticate");
 
-module.exports = { isValidId, validateBody, authenticate };
+module.exports = { isValidId, validateBody, authenticate, passport };
