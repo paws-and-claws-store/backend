2f2e1dd6c23044c7e94688759900240b8ffc87a2
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2024-02-02 16:08:29 +0200

    refactoring code for filters category logic

--- helpers/pagination.js
+++ helpers/pagination.js
@@ -5,6 +5,9 @@ const brandsCount = require('./brandsCount');
 const minMaxPriceRange = require('./minMaxPriceRange');
 const categories = require('./categories');
 
+const DEFAULT_MIN_PRICE = 0;
+const DEFAULT_MAX_PRICE = 10000000000;
+
 module.exports = async ({
   Model,
   filter = {},
@@ -12,8 +15,8 @@ module.exports = async ({
   limit = LIMIT_PAGINATION,
   collectionLinks = [],
   sortBy = 'cheap', // this fiels is required, beacause load more work uncorrectly response goods the same goods
-  minPrice = 0,
-  maxPrice = 10000000000,
+  minPrice = DEFAULT_MIN_PRICE,
+  maxPrice = DEFAULT_MAX_PRICE,
   brands, // string of brands, split by comma
   availability,
   aggregate,
@@ -44,11 +47,14 @@ module.exports = async ({
         }),
       );
 
+      const isPriceRangeSet =
+        minPrice !== DEFAULT_MIN_PRICE && maxPrice !== DEFAULT_MAX_PRICE ? true : false;
+
       data.totalDocs = result.length;
       data.brands = brandsCount(result);
       data.brandsDefault = brandsCount(resultDefault);
       data.minMax = minMaxPriceRange(resultDefault);
-      data.categories = categories(resultDefault);
+      data.categories = categories(resultDefault, isPriceRangeSet ? result : undefined);
     } else {
       result = await Model.find(filter, '-min_sale').populate(collectionLinks.join(' '));
       data.totalDocs = await Model.count(filter);
