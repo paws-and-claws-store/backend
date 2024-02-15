43241cbf60f757a6b2acdcf4073f8110530d6909
Artem Liaschenko <112252037+Artem4535@users.noreply.github.com>
2024-02-15 19:50:30 +0200

    fix

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
